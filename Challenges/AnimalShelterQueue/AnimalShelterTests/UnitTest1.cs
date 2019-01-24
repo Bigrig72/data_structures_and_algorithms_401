dab56616f96fc796fc30e1f0b372188a9e5540eb
Regan Dufort <regan.t.dufort@gmail.com>
2019-01-24 07:44:11 -0800

    initial setup

---
+++ Challenges/AnimalShelterQueue/AnimalShelterTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using fifo_animal_shelter.classes;
+
+namespace AnimalShelterTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
