a61832204a2deab5fd1ec15884e47a23d366cd44
Regan Dufort <regan.t.dufort@gmail.com>
2019-01-28 20:36:03 -0800

    started fizz buzz

---
+++ Challenges/Trees-FizzBuzz/FizzBuzzBinarfyTree/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Trees.classes;
+
+namespace FizzBuzzBinarfyTree
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+        }
+
+        public static void FizzBuzzTree(BinaryTree binaryTree)
+        {
+            if(binaryTree.Top == null)
+            {
+                return;
+            }
+            if(binaryTree.Top.Left.Value % 3 == 0 & binaryTree.Top.Left.Value % 5 == 0)
+            {
+                binaryTree.Top.Left.Value = "FizzBuzz";
+            }
+        }
+    }
+}
