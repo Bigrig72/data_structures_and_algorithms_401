d2162c0676f852b23b89ca1a5370f42ea5ddd67e
Regan Dufort <regan.t.dufort@gmail.com>
2019-02-11 20:50:04 -0800

    test done and algo working

--- Challenges/AncestryTest/UnitTest1.cs
+++ Challenges/AncestryTest/UnitTest1.cs
@@ -1,14 +1,39 @@
 using System;
 using Xunit;
+using Trees.Classes;
+using AncestryRelationship;
 
 namespace AncestryTest
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void TestTrueIfNodeHasCousin()
         {
+            Node root = new Node(15);
+            root.Left = new Node(20);
+            root.Right = new Node(30);
+            root.Left.Left = new Node(25);
+            root.Left.Right = new Node(35);
+            root.Right.Left = new Node(10);
+            root.Right.Right = new Node(19);
 
+            var cousin = Program.IsAncestor(root, root.Left, root.Right);
+            Assert.True(cousin);
+        }
+        [Fact]
+        public void TestTrueIfNodeHasCousin2()
+        {
+            Node root = new Node(15);
+            root.Left = new Node(20);
+            root.Right = new Node(30);
+            root.Left.Left = new Node(25);
+            root.Left.Right = new Node(35);
+            root.Right.Left = new Node(10);
+            root.Right.Right = new Node(19);
+
+            var cousin = Program.IsAncestor(root, root.Left.Left, root.Right);
+            Assert.False(cousin);
         }
     }
 }
