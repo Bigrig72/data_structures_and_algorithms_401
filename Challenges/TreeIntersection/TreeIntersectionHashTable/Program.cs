d9cfd9ba1dd57add646858fc347176405c37ec30
Regan Dufort <regan.t.dufort@gmail.com>
2019-02-19 22:12:33 -0800

    added comments and tests

--- Challenges/TreeIntersection/TreeIntersectionHashTable/Program.cs
+++ Challenges/TreeIntersection/TreeIntersectionHashTable/Program.cs
@@ -9,8 +9,6 @@ namespace TreeIntersectionHashTable
     {
         static void Main(string[] args)
         {
-          
-
             Console.WriteLine("Test Binary Tree");
             BinaryTree root = new BinaryTree();
             root.Top = new Node(40);
@@ -20,7 +18,6 @@ namespace TreeIntersectionHashTable
             root.Top.Left.Right = new Node(35);
             root.Top.Right.Left = new Node(10);
             root.Top.Right.Right = new Node(19);
-
 
             Console.WriteLine("Test Binary Tree");
             BinaryTree root2 = new BinaryTree();
@@ -44,30 +41,34 @@ namespace TreeIntersectionHashTable
 
             }
         }
-
+        /// <summary>
+        /// This method wil take in 2 lists that will store the preordered binary trees. 
+        ///  Next the values at each tree are hashed into the hash table.
+        ///  The first for loop will add the first list into the hash table
+        ///  The second list we will create a index for the hash table 
+        ///  We will do a check using the contains method to see if both values that were hashed are contained in the table
+        ///  If they are contained we add to the new final list, otherwise add to table
+        /// </summary>
+        /// <param name="storedTreeValues"></param>
+        /// <param name="storedTreeValues2"></param>
+        /// <param name="tree1"></param>
+        /// <param name="tree2"></param>
+        /// <returns></returns>
         public static List<int> TreeIntersection(List<int> storedTreeValues, List<int> storedTreeValues2, BinaryTree tree1, BinaryTree tree2)
         {
             // Setting up tree 1 iteration to final list
             HashTableSetup table = new HashTableSetup(1024);
-
-
             tree1.PreOrder(tree1.Top, storedTreeValues);
             tree2.PreOrder(tree2.Top, storedTreeValues2);
-
-            //storedTreeValues.Add(tree1.Top.Value);
-            //storedTreeValues.Add(tree2.Top.Value);
 
             List<int> finalList = new List<int>();
 
             int index;
-          
-           
+
             for (int i = 0; i < storedTreeValues.Count; i++)
             {
-                //index = table.Hash(storedTreeValues[i].ToString());
                 table.Add(storedTreeValues[i].ToString(), storedTreeValues[i].ToString());
-             
-            
+
                  for (int j = 0; j < storedTreeValues2.Count; j++)
                  {
                     index = table.Hash(storedTreeValues2[j].ToString());
@@ -82,16 +83,6 @@ namespace TreeIntersectionHashTable
                  }
             }
             return finalList;
-        }
-
-        //public void PreOrder(Node node, HashTableSetup table)
-        //{
-        //    if (node != null)
-        //    {
-        //        table.Add(node.Value, 0);
-        //        PreOrder(node.Left, table);
-        //        PreOrder(node.Right, table);
-        //    }
-        //}
+        }        
     }
 }
