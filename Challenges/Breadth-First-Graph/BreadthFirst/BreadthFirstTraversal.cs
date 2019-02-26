40a1098a491d85eda09669bdbe4d8328e3e8909b
Regan Dufort <regan.t.dufort@gmail.com>
2019-02-26 12:26:04 -0800

    Finished testing for breadth first

---
+++ Challenges/Breadth-First-Graph/BreadthFirst/BreadthFirstTraversal.cs
@@ -0,0 +1,47 @@
+﻿using AdjacentListGraph;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BreadthFirst
+{
+    public class BreadthFirstTraversal
+    {
+        public HashSet<T> BFS<T>(Graph<T> graph, T start, Action<T> preVisit = null)
+        {
+            var visited = new HashSet<T>();
+
+            if (!graph.AdjacencyList.ContainsKey(start))
+            {
+                return visited;
+            }
+            var queue = new Queue<T>();
+            queue.Enqueue(start);
+
+            while (queue.Count > 0)
+            {
+                var vertex = queue.Dequeue();
+                if (visited.Contains(vertex))
+
+                    continue;
+
+                if (preVisit != null)
+
+                    preVisit(vertex);
+
+                visited.Add(vertex);
+
+                foreach (var neighbor in graph.AdjacencyList[vertex])
+                {
+                    if (!visited.Contains(neighbor))
+                    {
+                        queue.Enqueue(neighbor);
+                    }
+                }
+
+            }
+            return visited;
+
+        }
+    }
+}
--- Challenges/Breadth-First-Graph/BreadthFirst/Program.cs
+++ Challenges/Breadth-First-Graph/BreadthFirst/Program.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using AdjacentListGraph;
+using System;
+using System.Collections.Generic;
 
 namespace BreadthFirst
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var vertices = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var edges = new[]{Tuple.Create(1,2), Tuple.Create(1,3),
+                Tuple.Create(2,4), Tuple.Create(3,5), Tuple.Create(3,6),
+                Tuple.Create(4,7), Tuple.Create(5,7), Tuple.Create(5,8),
+                Tuple.Create(5,6), Tuple.Create(8,9), Tuple.Create(9,10)};
+
+            var graph = new Graph<int>(vertices, edges);
+            var algorithms = new BreadthFirstTraversal();
+
+            Console.WriteLine(string.Join(", ", algorithms.BFS(graph, 1)));
+
         }
+       
     }
 }
